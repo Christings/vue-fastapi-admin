fb445b8411bb46220b97865954ea957921c34873
zhiting.gong <zhiting.gong@samsung.com>
2024-08-26 14:49:51 +0800

    feat: delelte unique in webhook

--- app/models/webhook.py
+++ app/models/webhook.py
@@ -1,14 +1,11 @@
 from tortoise import fields
 
-from app.schemas.menus import MenuType
-
 from .base import BaseModel, TimestampMixin
-from .enums import MethodType
 
 
 class Webhook(BaseModel, TimestampMixin):
-    project = fields.CharField(max_length=64, unique=True, description="项目名称")
-    name = fields.CharField(max_length=64, unique=True, description="webhook名称")
+    project = fields.CharField(max_length=64, description="项目名称")
+    name = fields.CharField(max_length=64, description="webhook名称")
     webhook = fields.CharField(max_length=500, null=True, description="webhook url")
     switch = fields.BooleanField(default=False, description="是否开启", index=True)
 
